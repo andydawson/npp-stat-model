2208c6d28b6ef71849c9094de42a672899ea28df
Andria Dawson <andria.dawson@gmail.com>
2019-03-01 10:30:47 -0700

    Adding model run files

---
+++ r/run_rw_model_NORTHROUND_STAN.R
@@ -0,0 +1,36 @@
+library(rstan)
+
+# source('config_HMC')
+dvers="v0.1"
+mvers="v0.1"
+site = "NORTHROUND"
+
+fname_data = paste0('tree_data_NORTHROUND_STAN_', dvers)
+fname_model = "ring_model_t_pdbh_sigd_STAN"
+
+load(paste0('output/ring_model_t_pdbh_HMC_NOCOVAR_v3.0.Rdata'))
+
+col_names = sapply(strsplit(colnames(out), '\\['), function(x) x[[1]])
+sig_d_obs = mean(out[,which(col_names=="sig_d_obs")])
+
+
+dat = readRDS(paste0('data/dump/', fname_data, '.RDS'))
+dat$sig_d_obs = sig_d_obs
+#######################################################################################################################################
+# full model but with zero covariance; not efficient
+#######################################################################################################################################
+
+compiled <- stan_model(file = paste0('models/stan/ring_model_t_pdbh_sigd_STAN.stan'))
+
+fit <- sampling(compiled, 
+                data = dat, 
+                iter = 5000, 
+                chains = 1,
+                verbose=TRUE)
+rm(compiled)
+
+post=rstan::extract(fit)
+rm(fit)
+
+
+save(post, file = paste0('output/', site, '/', fname_model, '_', mvers, '.Rdata'))
